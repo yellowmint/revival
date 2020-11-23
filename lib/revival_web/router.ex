2745ca29a3296d3a319fd9b3b7186b8067240ab3
yellowmint <michnajacek0@gmail.com>
2020-11-23 19:54:47 +0100

    Mount router

--- lib/revival_web/router.ex
+++ lib/revival_web/router.ex
@@ -18,6 +18,7 @@ defmodule RevivalWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
