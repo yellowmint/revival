71673527823ec4d5e82614424fb65172c92f21d5
yellowmint <michnajacek0@gmail.com>
2020-11-23 19:09:25 +0100

    Set db connection for test config

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,10 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :revival, Revival.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("PGUSER"),
+  password: System.get_env("PGPASSWORD"),
+  hostname: System.get_env("PGHOST"),
   database: "revival_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
