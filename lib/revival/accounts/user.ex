b527f43f06164b7b57dd9324f65af5c0ef01a618
yellowmint <michnajacek0@gmail.com>
2020-11-23 19:58:36 +0100

    Use uuid as primary key in user model

--- lib/revival/accounts/user.ex
+++ lib/revival/accounts/user.ex
@@ -2,6 +2,8 @@ defmodule Revival.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+
   schema "users" do
     field :email, :string
     field :name, :string
--- priv/repo/migrations/20201123185156_create_users.exs
+++ priv/repo/migrations/20201123185156_create_users.exs
@@ -2,7 +2,8 @@ defmodule Revival.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :uuid, primay_key: true
       add :email, :string
       add :provider, :string
       add :name, :string
