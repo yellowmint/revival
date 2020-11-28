48367a4ff235d498f4516f90a23397779c698887
yellowmint <michnajacek0@gmail.com>
2020-11-28 14:59:52 +0100

    Configure scss modules

--- assets/js/game/board.tsx
+++ assets/js/game/board.tsx
@@ -19,10 +19,10 @@ export const Board: React.FC<BoardProps> = (props: BoardProps) => {
 
     return (
         <section className={styles.board}>
-            {board.fields.map(row =>
-                <div>
-                    {row.map(column =>
-                        <div>field</div>
+            {board.fields.map((row, rowIdx) =>
+                <div key={rowIdx}>
+                    {row.map((column, columnIdx) =>
+                        <div key={columnIdx}>field</div>
                     )}
                 </div>
             )}
