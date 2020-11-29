e46849a74ae1fd880865a898ea1f15b74a69c9af
yellowmint <michnajacek0@gmail.com>
2020-11-29 12:49:04 +0100

    Move js connection logic to separeate file

--- assets/js/game/game.tsx
+++ assets/js/game/game.tsx
@@ -1,33 +1,9 @@
-import React, {useEffect, useState} from "react"
-import {Channel, Socket} from "phoenix"
-import {Board, TBoard} from "./board"
-
-type Game = {
-    board: TBoard
-}
+import React from "react"
+import {Board} from "./board"
+import {useConnectionLogic} from "./useConnectionLogic"
 
 export const Game = () => {
-    const [game, setGame] = useState<Game>()
-    const [channel, setChannel] = useState<Channel>()
-
-    useEffect(() => {
-        const tokenTag = document.querySelector('meta[name="auth-token"]') as HTMLMetaElement
-        const authParams = tokenTag ? {token: tokenTag.content} : {}
-
-        const socket = new Socket('/socket', {params: authParams})
-        socket.connect()
-
-        const {pathname} = window.location
-        const id = pathname.substring(pathname.lastIndexOf('/') + 1)
-
-        const channelHandle = socket.channel("game:" + id)
-        channelHandle.join()
-            .receive("ok", resp => setGame(resp))
-            .receive("error", resp => console.log("Unable to join", resp))
-
-        setChannel(channelHandle)
-        return () => socket.disconnect()
-    }, [])
+    const {game, channel} = useConnectionLogic()
 
     if (!game) return <div>Loading</div>
 
---
+++ assets/js/game/useConnectionLogic.ts
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+import {Channel, Socket} from "phoenix";
+import {TBoard} from "./board";
+
+type Game = {
+    board: TBoard
+}
+
+export const useConnectionLogic = () => {
+    const [game, setGame] = useState<Game>()
+    const [channel, setChannel] = useState<Channel>()
+
+    useEffect(() => {
+        const tokenTag = document.querySelector('meta[name="auth-token"]') as HTMLMetaElement
+        const authParams = tokenTag ? {token: tokenTag.content} : {}
+
+        const socket = new Socket('/socket', {params: authParams})
+        socket.connect()
+
+        const {pathname} = window.location
+        const id = pathname.substring(pathname.lastIndexOf('/') + 1)
+
+        const channelHandle = socket.channel("game:" + id)
+        channelHandle.join()
+            .receive("ok", resp => setGame(resp))
+            .receive("error", resp => console.log("Unable to join", resp))
+
+        setChannel(channelHandle)
+        return () => socket.disconnect()
+    }, [])
+
+    return {game, channel}
+}
