e47332faf894bb981a989c31bbaceab4a3b740fc
yellowmint <michnajacek0@gmail.com>
2020-12-08 01:08:20 +0100

    Not every corpse returns to shop

--- lib/revival/games/shop.ex
+++ lib/revival/games/shop.ex
@@ -9,24 +9,122 @@ defmodule Revival.Games.Shop do
     defstruct [:kind, :level, :count, :price]
   end
 
-  defp new_good("satyr", 1), do: %Good{kind: "satyr", level: 1, price: %{money: 15, mana: 0}}
-  defp new_good("satyr", 2), do: %Good{kind: "satyr", level: 2, price: %{money: 35, mana: 0}}
-  defp new_good("satyr", 3), do: %Good{kind: "satyr", level: 3, price: %{money: 70, mana: 0}}
-  defp new_good("golem", 1), do: %Good{kind: "golem", level: 1, price: %{money: 20, mana: 0}}
-  defp new_good("golem", 2), do: %Good{kind: "golem", level: 2, price: %{money: 45, mana: 0}}
-  defp new_good("golem", 3), do: %Good{kind: "golem", level: 3, price: %{money: 90, mana: 0}}
-  defp new_good("minotaur", 1), do: %Good{kind: "minotaur", level: 1, price: %{money: 30, mana: 5}}
-  defp new_good("minotaur", 2), do: %Good{kind: "minotaur", level: 2, price: %{money: 70, mana: 15}}
-  defp new_good("minotaur", 3), do: %Good{kind: "minotaur", level: 3, price: %{money: 140, mana: 35}}
-  defp new_good("wraith", 1), do: %Good{kind: "wraith", level: 1, price: %{money: 80, mana: 20}}
-  defp new_good("wraith", 2), do: %Good{kind: "wraith", level: 2, price: %{money: 150, mana: 50}}
-  defp new_good("wraith", 3), do: %Good{kind: "wraith", level: 3, price: %{money: 300, mana: 80}}
+  defp new_good("satyr", 1),
+       do: %Good{
+         kind: "satyr",
+         level: 1,
+         price: %{
+           money: 15,
+           mana: 0
+         }
+       }
+  defp new_good("satyr", 2),
+       do: %Good{
+         kind: "satyr",
+         level: 2,
+         price: %{
+           money: 35,
+           mana: 0
+         }
+       }
+  defp new_good("satyr", 3),
+       do: %Good{
+         kind: "satyr",
+         level: 3,
+         price: %{
+           money: 70,
+           mana: 0
+         }
+       }
+  defp new_good("golem", 1),
+       do: %Good{
+         kind: "golem",
+         level: 1,
+         price: %{
+           money: 20,
+           mana: 0
+         }
+       }
+  defp new_good("golem", 2),
+       do: %Good{
+         kind: "golem",
+         level: 2,
+         price: %{
+           money: 45,
+           mana: 0
+         }
+       }
+  defp new_good("golem", 3),
+       do: %Good{
+         kind: "golem",
+         level: 3,
+         price: %{
+           money: 90,
+           mana: 0
+         }
+       }
+  defp new_good("minotaur", 1),
+       do: %Good{
+         kind: "minotaur",
+         level: 1,
+         price: %{
+           money: 30,
+           mana: 5
+         }
+       }
+  defp new_good("minotaur", 2),
+       do: %Good{
+         kind: "minotaur",
+         level: 2,
+         price: %{
+           money: 70,
+           mana: 15
+         }
+       }
+  defp new_good("minotaur", 3),
+       do: %Good{
+         kind: "minotaur",
+         level: 3,
+         price: %{
+           money: 140,
+           mana: 35
+         }
+       }
+  defp new_good("wraith", 1),
+       do: %Good{
+         kind: "wraith",
+         level: 1,
+         price: %{
+           money: 80,
+           mana: 20
+         }
+       }
+  defp new_good("wraith", 2),
+       do: %Good{
+         kind: "wraith",
+         level: 2,
+         price: %{
+           money: 150,
+           mana: 50
+         }
+       }
+  defp new_good("wraith", 3),
+       do: %Good{
+         kind: "wraith",
+         level: 3,
+         price: %{
+           money: 300,
+           mana: 80
+         }
+       }
 
   def new_shop("classic") do
     %Shop{
       goods: [
-        new_good("satyr", 1) |> Map.put(:count, 5),
-        new_good("golem", 1) |> Map.put(:count, 5),
+        new_good("satyr", 1)
+        |> Map.put(:count, 5),
+        new_good("golem", 1)
+        |> Map.put(:count, 5),
       ]
     }
   end
@@ -62,8 +160,13 @@ defmodule Revival.Games.Shop do
   end
 
   defp corpse_to_good(corpse, shop) do
-    good = new_good(corpse.kind, corpse.level) |> Map.put(:count, 1)
-    add_to_shop(shop, good)
+    cond do
+      corpse.live < -20 -> shop
+      true ->
+        good = new_good(corpse.kind, corpse.level)
+               |> Map.put(:count, 1)
+        add_to_shop(shop, good)
+    end
   end
 
   defp add_to_shop(shop, good) do
@@ -84,7 +187,8 @@ defmodule Revival.Games.Shop do
       true -> Enum.random([1, 2, 3])
     end
 
-    good = new_good(kind, level) |> Map.put(:count, 1)
+    good = new_good(kind, level)
+           |> Map.put(:count, 1)
     add_to_shop(shop, good)
   end
 end
